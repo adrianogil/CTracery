50491ccc19bafcd9fb54da7bb8784906311a69a3
Adriano Gil <adriano.gil@indt.org.br>
2015-10-04 03:18:11 -0400

    Simple grammar to story generation.

---
+++ Grammar.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grammar 
+{
+
+	Dictionary<string, string[]> evaluationsByTags;
+
+	public Grammar()
+	{
+		evaluationsByTags = new Dictionary<string, string[]>();
+	}	
+
+	public void AddTag(string tagName, string[] textEvaluations)
+	{
+		evaluationsByTags[tagName] = textEvaluations;
+	}
+
+	public string Evaluate(string taggedText)
+	{
+		List<string> tags = ParseTagsFrom(taggedText);
+
+		if (tags.Count == 0) // No tags means a plain and normal text
+		{
+			return taggedText;
+		}
+
+		List<string> tagsEvaluated = Evaluate(tags);
+
+		string textEvaluated = taggedText;
+
+		for (int s = 0; s < tags.Count; s++)
+		{
+			textEvaluated = textEvaluated.Replace("#" + tags[s] + "#", tagsEvaluated[s]);
+		}
+
+		return textEvaluated;
+	}
+
+	private List<string> Evaluate(List<string> tagList)
+	{
+		List<string> evaluatedTagList = new List<string>();
+
+		foreach (string tag in tagList) 
+		{
+			if (evaluationsByTags.ContainsKey(tag))
+			{
+				string taggedText = evaluationsByTags[tag].GetRandom<string>();
+				evaluatedTagList.Add(Evaluate(taggedText));
+			}
+		}
+
+		return evaluatedTagList;
+	}
+
+	/// <summary>
+	/// Tags are between ##
+	/// #tag1##tag2#
+	/// </summary>
+	private List<string> ParseTagsFrom(string taggedText)
+	{
+		List<string> tags = new List<string>();
+
+		string tag = "";
+		bool inTag = false;
+
+		for (int i = 0; i < taggedText.Length; i++)
+		{
+			if (taggedText[i] == '#')
+			{
+				if (inTag)
+				{
+					tags.Add(tag);
+				}
+
+				tag = "";
+				inTag = !inTag;
+			}
+			else if (inTag)
+			{
+				tag += taggedText[i];
+			}
+
+		}
+
+		return tags;
+	}
+}
---
+++ GrammarTest.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrammarTest : MonoBehaviour {
+
+	private Grammar grammar;
+
+	// Use this for initialization
+	void Start () {
+		grammar = new Grammar();
+
+		grammar.AddTag("story", new string[] {
+			"#story_beginning# #story_problem# #story_climax# #story_ending#"
+			});
+		grammar.AddTag("story_beginning", new string[] {
+			"Once upon a time there was a valiant #animal#"
+			});
+		grammar.AddTag("story_problem", new string[] {
+			"that never #difficulty_verb#.",
+			"that one day heard some strange words: #strange_calling#"
+			});
+		grammar.AddTag("story_climax", new string[] {
+			"Suddenly, he decided to #resolution_verb#."
+			});
+		grammar.AddTag("story_ending", new string[] {
+			"Finally he could #result_verb# without worries."
+			});
+		grammar.AddTag("difficulty_verb", new string[] {"slept", "danced", "talked"});
+		grammar.AddTag("resolution_verb", new string[] {"run", "sing", "give up"});
+		grammar.AddTag("result_verb", new string[] {"sleep", "dance", "talk freely"});
+
+		grammar.AddTag("strange_calling", new string[] {"Hello #name#!", "Hello my #writer_object#!"});
+		grammar.AddTag("animal", new string[] {"dolphin", "dog", "cat", "lamb", "lion"});
+		grammar.AddTag("name", new string[] {"Mr. Gil", "Madame", "Masked Man"});
+		grammar.AddTag("writer_object", new string[] {"text", "book", "beloved code"});
+
+
+
+		InvokeRepeating("TestEvaluation", 1f, 2f);
+	}
+	
+	// Update is called once per frame
+	void TestEvaluation () 
+	{
+		Debug.Log(grammar.Evaluate("#story#"));
+	}
+}
